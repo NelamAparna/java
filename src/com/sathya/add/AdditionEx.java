11544485ebf53f6a7bda8881e1a2438a8856d24b
NelamAparna <nelamaparna9@gmail.com>
2023-05-31 14:55:19 +0530

    Addition of two numbers

---
+++ src/com/sathya/add/AdditionEx.java
@@ -0,0 +1,12 @@
+package com.sathya.add;
+
+public class AdditionEx {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=20;
+		int res=num1+num2;
+		System.out.println("Addition of two numbers" +res);
+	}
+
+}
